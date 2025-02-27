38ad377906a1c5b33966d2bae473909e311abd36
Karthikeyan <karthikeyan@pippintitle.com>
2025-02-27 16:13:07 +0530

    new script

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Hello, pippin this new script JavaScript!");
+//console.log("Hello, pippin this new script JavaScript!");
+const http = require("http");
+
+// Create an HTTP server
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello, World!\n");
+});
+
+// Define the port
+const PORT = 3000;
+
+// Start the server
+server.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
