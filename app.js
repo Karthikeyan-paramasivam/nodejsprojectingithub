ef2f5bd893276a8067dad2dc123f01b1a9660a87
Karthikeyan <karthikeyan@pippintitle.com>
2025-03-03 15:43:02 +0530

    welcome to india

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const http = require("http");
 // Create an HTTP server
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello,Pippin!\n");
+  res.end("Hello,Pippin welcome to indai!\n");
 });
 
 // Define the port
