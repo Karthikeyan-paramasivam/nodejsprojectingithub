43a164397a0e8a16f34d98745e5c8a18b88a17a1
Karthikeyan <karthikeyan@pippintitle.com>
2025-03-03 13:10:27 +0530

    new upate

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const http = require("http");
 // Create an HTTP server
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello, Welcome to pippin for new person my update for document!\n");
+  res.end("Hello, pippin!\n");
 });
 
 // Define the port
