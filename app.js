6be84618214bd666c8eb233f256ed1f6aa323f25
Karthikeyan <karthikeyan@pippintitle.com>
2025-07-30 16:04:00 +0530

    finaltest

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const server = http.createServer((req, res) => {
     fs.readFile(filePath, (err, content) => {
       if (err) {
         res.writeHead(500);
-        res.end("Error loading ghgfhgfhfghdddd  fgfdg hfhfghgfh   HTML file.");
+        res.end("Error HTML file.");
       } else {
         res.writeHead(200, { "Content-Type": "text/html" });
         res.end(content);
