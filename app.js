b6c48d1436c095617f6fb1df3fc50a6406639539
Karthikeyan <karthikeyan@pippintitle.com>
2025-07-31 14:47:37 +0530

    today

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const server = http.createServer((req, res) => {
     fs.readFile(filePath, (err, content) => {
       if (err) {
         res.writeHead(500);
-        res.end("Error HTML File Now created.");
+        res.end("Error HTML File Now created for test.");
       } else {
         res.writeHead(200, { "Content-Type": "text/html" });
         res.end(content);
