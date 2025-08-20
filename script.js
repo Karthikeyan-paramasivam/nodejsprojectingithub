7c8acc2639529d9c8e4ffdf38e1c6d780cc71013
Karthikeyan <karthikeyan@pippintitle.com>
2025-08-20 16:42:22 +0530

    250

--- script.js
+++ script.js
@@ -104,7 +104,7 @@ function showContent(type) {
     style="padding: 8px; width: 110px; border-radius: 5px; border: 1px solid #ccc;">
 </div>
 
-<div style="display: flex; flex-direction: column; margin: 10px 0;">
+<div style="display: flex; flex-direction: column; margin: 5px 0;">
   <label for="remarks">Remarks</label>
   <textarea id="remarks" rows="3" placeholder="Enter your remarks here..."
     style="padding: 5px; width: 250px; border-radius: 5px; border: 1px solid #ccc; resize: vertical;"></textarea>
