cd45cccfd1dde1ca6e19887876e6312027df312e
Karthikeyan <karthikeyan@pippintitle.com>
2025-08-20 15:40:57 +0530

    233

--- script.js
+++ script.js
@@ -169,14 +169,7 @@ function showContent(type) {
           <label for="monitor_ser_num">Monitor Serial Number-2</label>
           <input id="ser_num" type="text" style="padding: 8px; width: 150px; border-radius: 5px; border: 1px solid #ccc;">
           </div>
-          
-          <!-- Remove button -->
-          <div style="display: flex; flex-direction: column; justify-content: flex-end;">
-          <button type="button" onclick="removeMonitor('monitor2-block')"
-          style="padding: 6px 12px; background-color: green; color: white; border: none; border-radius: 6px; cursor: pointer;">
-          Remove
-          </button>
-          </div>
+                    
           </div>
 
           <div id="monitor3-block" style="display: flex; align-items: center; gap: 20px; margin: 10px 0;">
@@ -221,13 +214,6 @@ function showContent(type) {
             <input id="Headset_serial" type="text" style="padding: 8px; width: 150px; border-radius: 5px; border: 1px solid #ccc;">
           </div>
 
-          <!-- Remove button -->
-          <div style="display: flex; flex-direction: column; justify-content: flex-end;">
-          <button type="button" onclick="removeMonitor('headset-block')"
-          style="padding: 6px 12px; background-color: green; color: white; border: none; border-radius: 6px; cursor: pointer;">
-          Remove
-          </button>
-          </div>
           </div>
 
           <div id="mouse-block" style="display: flex; align-items: center; gap: 20px; margin: 10px 0;">
@@ -252,13 +238,6 @@ function showContent(type) {
             <input id="mouse_serial" type="text" style="padding: 8px; width: 150px; border-radius: 5px; border: 1px solid #ccc;">
           </div>
 
-          <!-- Remove button -->
-          <div style="display: flex; flex-direction: column; justify-content: flex-end;">
-          <button type="button" onclick="removeMonitor('mouse-block')"
-          style="padding: 6px 12px; background-color: green; color: white; border: none; border-radius: 6px; cursor: pointer;">
-          Remove
-          </button>
-          </div>
           </div>
 
           <div style="text-align: left; margin-bottom: 20px;">
